95511ea2a10b25426693e14d50b9e3d490aa10cc
fartcoin-project <fartcoinproject@gmail.com>
2023-10-09 15:30:54 +0200

    KT merged in other

--- src_framework/config.h
+++ src_framework/config.h
@@ -1,8 +1,10 @@
-// This is the basic config file that defines which
-// LILYGO product you are compiling for
+// => Hardware select
+// #define LILYGO_WATCH_2019_WITH_TOUCH     // To use T-Watch2019 with touchscreen, please uncomment this line
+// #define LILYGO_WATCH_2019_NO_TOUCH       // To use T-Watch2019 Not touchscreen , please uncomment this line
+#define LILYGO_WATCH_2020_V3                // To use a different watch version, comment this
 
-#define LILYGO_WATCH_2020_V3              // Use T-Watch2020
-#define LILYGO_WATCH_LVGL 
+
+// => Function select
+#define LILYGO_WATCH_LVGL                   // To use LVGL, you need to enable the macro LVGL
 
 #include <LilyGoWatch.h>
-TTGOClass *ttgo;
